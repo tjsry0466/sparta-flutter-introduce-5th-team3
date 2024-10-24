d4238e54da46c4c7a93de4f1818874399ddcb13a
seonkyo <tjs0466@naver.com>
2024-10-24 11:53:53 +0900

    feat: add global style

---
+++ lib/components/common/AppStyle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle sectionTitle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const EdgeInsets sectionPadding =
+      EdgeInsets.only(top: 26, bottom: 24, left: 24, right: 24);
+  static const double defaultSpacing = 8.0;
+}
